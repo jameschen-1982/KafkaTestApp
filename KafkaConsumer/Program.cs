e84bf36bdfb9ac52770fcb2f103cee1a8a099f1d
James Chen <james.chen@standards.org.au>
2024-08-18 15:39:11 +1000

    Set up Kafka code

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using Confluent.Kafka;
+using Microsoft.Data.SqlClient;
+
+namespace KafkaConsumer;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("NewTopic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    // Prevent the process from terminating.
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+
+                            await using SqlConnection connection = new SqlConnection(
+                                "Data source=localhost; Initial Catalog=kafka_test; User Id=kafka_user; Password=Abc123456!;TrustServerCertificate=True;");
+                            Console.WriteLine("\nQuery data example:");
+                            Console.WriteLine("=========================================\n");
+
+                            var sql = "InsertUser";
+
+                            await using SqlCommand cmd = new SqlCommand(sql, connection);
+                            cmd.CommandType = CommandType.StoredProcedure;
+
+                            cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = "John";
+                            cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = "Smith";
+
+                            await connection.OpenAsync(cts.Token);
+                            await cmd.ExecuteNonQueryAsync(cts.Token);
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+using Microsoft.Data.SqlClient;
+
+namespace KafkaProducer;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var conf = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        Action<DeliveryReport<string, string>> handler = r =>
+            Console.WriteLine(!r.Error.IsError
+                ? $"Delivered message to {r.TopicPartitionOffset}"
+                : $"Delivery Error: {r.Error.Reason}");
+
+        using (var p = new ProducerBuilder<string, string>(conf).Build())
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                p.Produce("NewTopic", new Message<string, string> { Key = i.ToString(), Value = i.ToString() }, handler);
+            }
+
+            // wait for up to 10 seconds for any inflight messages to be delivered.
+            p.Flush(TimeSpan.FromSeconds(10));
+        }
+    }
+}
